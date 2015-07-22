4bd37f4c33836fd8b5f5f3562c9b2822dbe45e43
faridmax <faridmax@hotmail.com>
2015-07-22 11:20:26 +0200

    add translator

---
+++ js/translator.js
@@ -0,0 +1,3 @@
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'fr', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, multilanguagePage: true, gaTrack: true, gaId: 'UA-64770487-1'}, 'google_translate_element');
+}
